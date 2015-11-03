fec2513a40694a20296edb48dfe36922a5fd88f3
Kare Morstol <kare@nottoobadsoftware.com>
2015-11-03 03:48:38 +0100

    Initial commit

---
+++ Moderator.swift/Moderator.swift.h
@@ -0,0 +1,19 @@
+//
+// Moderator.swift.h
+// Moderator.swift
+//
+// Created by Kåre Morstøl on 03.11.15.
+// Copyright © 2015 NotTooBad Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Moderator.swift.
+FOUNDATION_EXPORT double Moderator_swiftVersionNumber;
+
+//! Project version string for Moderator.swift.
+FOUNDATION_EXPORT const unsigned char Moderator_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moderator_swift/PublicHeader.h>
+
+
